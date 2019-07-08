b3f69b335d085fb31ab3f75d8e092c357418a25b
lq <lq@lanqiao.org>
2019-07-08 16:20:35 +0800

    第一次修改提交

---
+++ src/main/java/Servlet.java
@@ -0,0 +1,59 @@
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executor;
+
+public class Servlet extends HttpServer {
+    @Override
+    public void bind(InetSocketAddress inetSocketAddress, int i) throws IOException {
+        
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void setExecutor(Executor executor) {
+
+    }
+
+    @Override
+    public Executor getExecutor() {
+        return null;
+    }
+
+    @Override
+    public void stop(int i) {
+
+    }
+
+    @Override
+    public HttpContext createContext(String s, HttpHandler httpHandler) {
+        return null;
+    }
+
+    @Override
+    public HttpContext createContext(String s) {
+        return null;
+    }
+
+    @Override
+    public void removeContext(String s) throws IllegalArgumentException {
+
+    }
+
+    @Override
+    public void removeContext(HttpContext httpContext) {
+
+    }
+
+    @Override
+    public InetSocketAddress getAddress() {
+        return null;
+    }
+}
